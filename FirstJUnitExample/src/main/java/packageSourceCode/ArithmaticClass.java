6e97b8a3b87fffe1054b4b002dcd409b9d2e25eb
Md. Siam Ansary <siamansary.cse@gmail.com>
2020-11-04 01:16:36 +0600

    Update ArithmaticClass.java

--- FirstJUnitExample/src/main/java/packageSourceCode/ArithmaticClass.java
+++ FirstJUnitExample/src/main/java/packageSourceCode/ArithmaticClass.java
@@ -1,5 +1,25 @@
 package packageSourceCode;
 
 public class ArithmaticClass {
+	
+	public int methodAdd(int a, int b)
+	{
+		return a + b ;
+	}
+	
+	public int methodSub(int a, int b)
+	{
+		return a - b ;
+	}
+	
+	public int methodMul(int a, int b)
+	{
+		return a * b ;
+	}
+	
+	public int methodDiv(int a, int b)
+	{
+		return a / b ;
+	}
 
 }
