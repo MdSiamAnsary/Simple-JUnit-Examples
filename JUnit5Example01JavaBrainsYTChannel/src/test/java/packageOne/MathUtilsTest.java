96411f725fa41ea05ff499e0973ebd7a77721c1d
Md. Siam Ansary <siamansary.cse@gmail.com>
2020-11-04 22:54:06 +0600

    Maven Surefire Working

--- JUnit5Example01JavaBrainsYTChannel/src/test/java/packageOne/MathUtilsTest.java
+++ JUnit5Example01JavaBrainsYTChannel/src/test/java/packageOne/MathUtilsTest.java
@@ -4,15 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
+
 class MathUtilsTest {
 
 	@Test
 	public void addTestId1()
 	{
-		MathUtils mathUtils = new MathUtils();
-		int actual = mathUtils.add(10, 20);
-		int expected = 20;
-		assertEquals(expected, actual, "Add method");
+		MathUtils mathUtils= new MathUtils();
+		
+		int expected = 25;
+		int actual = mathUtils.add(12, 12);
+		
+		assertEquals(expected, actual);
 	}
 
 }
